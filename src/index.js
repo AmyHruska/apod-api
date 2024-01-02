a4e0785baa78383c5bd46782be2ba185ba18e7e3
AmyHruska <amyhruskacda@gmail.com>
2024-01-02 11:09:13 -0800

    add updated code with error handling

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import './css/styles.css';
 
 function getPicByDate(date) {
   let request = new XMLHttpRequest();
-  const apodUrl = `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${process.env.API_KEY}`
+  const apodUrl = `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${process.env.API_KEY}`;
 
   request.addEventListener("loadend", function () {
     const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response);
-      displayImage(response);
-    }
+    statusHandler(this.status, response);
   });
 
   request.open("GET", apodUrl, true);
@@ -20,15 +17,21 @@ function getPicByDate(date) {
 }
 
 
+function statusHandler(status, apiResponse) {
+ let responseHtml = document.querySelector('#showResponse');
+  if (status === 200) {
+    responseHtml.innerText = `Description: ${apiResponse.explanation}`;
+    displayImage(apiResponse);
+  } else if (status === 403) {
+    responseHtml.innerText = `${apiResponse.error.code} ${apiResponse.error.message}`;
+  } else {
+    responseHtml.innerText = `Error code: ${apiResponse.code} ${apiResponse.msg}`;
+  }
+}
+
 function displayImage(response) {
   const imgTag = `<img src="${response.url}"/>`
   document.getElementById('showResponse').insertAdjacentHTML('beforeend', imgTag);
-}
-
-
-function printElements(apiResponse) {
-  console.log(apiResponse);
-  document.querySelector('#showResponse').innerText = `Description: ${apiResponse.explanation}`;
 }
 
 function handleForm(e) {
@@ -38,16 +41,12 @@ function handleForm(e) {
   getPicByDate(date);
 }
 
-
-
-
-
 document.querySelector("#form").addEventListener("submit", handleForm);
 
 /*
 const renderCountry = (data, classname = '') => {
   const html = `
-  <article class="country ${classname}">
+    < article class="country ${classname}" >
     <img class="country__img" src="${data.flags.png}" />
     <div class="country__data">
       <h3 class="country__name">${data.name.common}</h3>
@@ -62,9 +61,10 @@ const renderCountry = (data, classname = '') => {
         Object.values(data.currencies)[0].name
       }</p>
     </div>
-  </article>
-  `;
+  </article >
+    `;
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
 }; 
+
+${process.env.API_KEY}
 */
